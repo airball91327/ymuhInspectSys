c9c10b3daf8f9c5281e76cb9e79b0f7ad204c07e
airball91327 <airball91327@hotmail.com>
2020-11-05 17:32:05 +0800

    20201105 update3.

---
+++ InspectSystem/InspectSystem/Areas/Admin/Controllers/InspectDocPreviewController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InspectSystem.Models;
+
+namespace InspectSystem.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class InspectDocPreviewController : Controller
+    {
+        private BMEDcontext db = new BMEDcontext();
+
+        // GET: Admin/InspectDocPreview
+        public async Task<ActionResult> Index()
+        {
+            // Get inspect area list.
+            var inspectAreas = db.InspectArea.Where(a => a.AreaStatus == true).ToList();
+            List<SelectListItem> listItem = new List<SelectListItem>();
+            foreach (var item in inspectAreas)
+            {
+                listItem.Add(new SelectListItem()
+                {
+                    Value = item.AreaId.ToString(),
+                    Text = item.AreaName
+                });
+            }
+            ViewData["AreaId"] = new SelectList(listItem, "Value", "Text");
+            return View();
+        }
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
