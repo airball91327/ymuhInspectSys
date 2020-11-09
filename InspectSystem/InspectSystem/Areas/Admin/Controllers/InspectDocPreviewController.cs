6edb2c2501b5d6444946c19a0f2c2eb6700d927f
airball91327 <airball91327@hotmail.com>
2020-11-09 10:32:09 +0800

    20201109 update1.

--- InspectSystem/InspectSystem/Areas/Admin/Controllers/InspectDocPreviewController.cs
+++ InspectSystem/InspectSystem/Areas/Admin/Controllers/InspectDocPreviewController.cs
@@ -20,7 +20,7 @@ namespace InspectSystem.Areas.Admin.Controllers
         public async Task<ActionResult> Index()
         {
             // Get inspect area list.
-            var inspectAreas = db.InspectArea.Where(a => a.AreaStatus == true).ToList();
+            var inspectAreas = await db.InspectArea.Where(a => a.AreaStatus == true).ToListAsync();
             List<SelectListItem> listItem = new List<SelectListItem>();
             foreach (var item in inspectAreas)
             {
@@ -34,7 +34,70 @@ namespace InspectSystem.Areas.Admin.Controllers
             return View();
         }
 
-        
+        // POST: Admin/InspectDocPreview/Index/5
+        [HttpPost]
+        public ActionResult Index(FormCollection fc)
+        {
+            var areaId = fc["AreaId"];
+            var shiftId = fc["ShiftId"];
+            return RedirectToAction("Preview", new { AreaId = areaId, ShiftId = shiftId });
+        }
+
+        // GET: Admin/InspectDocPreview/Preview/5
+        public ActionResult Preview(int? AreaId, int? ShiftId)
+        {
+            if (AreaId == null || ShiftId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var shiftsInAreas = db.ShiftsInAreas.Include(s => s.InspectArea).Include(s => s.InspectShift)
+                                                .Where(s => s.AreaId == AreaId && s.ShiftId == ShiftId).ToList();
+            // Set variables.
+            var shiftId = ShiftId;
+            var previewClasses = db.InspectClass.Where(c => c.AreaId == AreaId && c.ShiftId == ShiftId)
+                                                       .OrderBy(c => c.ClassOrder);
+            var shiftName = shiftsInAreas.FirstOrDefault().InspectShift.ShiftName;
+            var areaName = shiftsInAreas.FirstOrDefault().InspectArea.AreaName;
+            List<InspectClassVModel> inspectClassVs = new List<InspectClassVModel>();
+            InspectClassVModel classVModel;
+            //
+            foreach (var item in previewClasses)
+            {
+                // Insert values to classVModel.
+                classVModel = new InspectClassVModel();
+                classVModel.DocId = "00000000";
+                classVModel.AreaId = item.AreaId;
+                classVModel.ShiftId = item.ShiftId;
+                classVModel.ClassId = item.ClassId;
+                classVModel.ClassName = item.ClassName;
+                classVModel.ClassOrder = item.ClassOrder;
+                classVModel.IsSaved = false;
+                classVModel.CountErrors = 0;
+                inspectClassVs.Add(classVModel);
+            }
+            //
+            ViewBag.Header = areaName + "【" + shiftName + "】";
+            return View(inspectClassVs);
+        }
+
+        // Get: Admin/InspectDocPreview/GetClassContent/5
+        public ActionResult GetClassContent(string areaId, string shiftId, string classId)
+        {
+            int iAreaId = Convert.ToInt32(areaId);
+            int iShiftId = Convert.ToInt32(shiftId);
+            int iClassId = Convert.ToInt32(classId);
+            ViewBag.ClassName = db.InspectClass.Find(iAreaId, iShiftId, iClassId).ClassName;
+            // Get preview items and fields. 
+            ViewData["itemsPreview"] = db.InspectItem.Where(i => i.AreaId == iAreaId && i.ShiftId == iShiftId && i.ClassId == iClassId)
+                                                     .OrderBy(i => i.ItemOrder).ToList();
+            ViewData["fieldsPreview"] = db.InspectField.Where(i => i.AreaId == iAreaId && i.ShiftId == iShiftId && i.ClassId == iClassId)
+                                                       .ToList();
+            ViewData["dropdownsPreview"] = db.InspectFieldDropDown.Where(i => i.AreaId == iAreaId && i.ShiftId == iShiftId && i.ClassId == iClassId)
+                                                                  .ToList();
+            return PartialView();
+        }
+
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
