c1c0915949ec35d653821ebf0f4f2c6eca06189a
JimyHsu <airball91327@hotmail.com>
2021-03-23 17:06:19 +0800

    20210323 Update. Report controller.

--- InspectSystem/InspectSystem/Controllers/ReportController.cs
+++ InspectSystem/InspectSystem/Controllers/ReportController.cs
@@ -1,39 +1,115 @@
-﻿using System;
+﻿using InspectSystem.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace InspectSystem.Controllers
 {
+    [Authorize]
     public class ReportController : Controller
     {
+        private BMEDcontext db = new BMEDcontext();
         // GET: Report
         public ActionResult Index()
         {
+            ViewBag.AreaId = new SelectList(db.DEInspectArea, "AreaId", "AreaName");
             return View();
         }
 
-        /// <summary>
-        /// 依參數決定列印的報表。
-        /// </summary>
-        /// <param name="reportType">列印的報表種類</param>
-        /// <returns></returns>
-        // GET: Report/PrintReport
-        public ActionResult PrintReport(string reportType)
+        // GET: Report/Index2
+        public ActionResult Index2(ReportQryVModel qry)
         {
-            if (reportType == "DaliyReport")
+            int areaId = qry.AreaId;
+            int cycleId = qry.CycleId;
+            int classId = qry.ClassId;
+            DateTime applyDateFrom = qry.ApplyDateFrom;
+            DateTime applyDateTo = qry.ApplyDateTo.AddDays(1).AddSeconds(-1);
+            var cycle = db.DEInspectCycle.Find(cycleId);
+            //
+            if (cycle.CycleName.Contains("每日"))
             {
-                return View("DaliyReport");
-            }
-            else if (reportType == "MonthReport")
-            {
-                return View("MonthReport");
+                return RedirectToAction("GetDaliyReportWord");
             }
             else
             {
-                return View();
+                return RedirectToAction("GetMonthReportWord");
             }
         }
+
+        public ActionResult GetDaliyReportWord()
+        {
+            string fileName = "日報表";
+            Response.Clear();
+            Response.AddHeader("content-disposition", "attachment;filename=" + fileName + ".doc"); //fileName是word的檔名
+            Response.ContentEncoding = System.Text.Encoding.GetEncoding("utf-8");     //編碼utf-8
+            Response.ContentType = "application/vnd.ms-word";                         //讓瀏覽器知道是word檔
+            Response.Charset = "";
+
+            string myDoc = "";
+            StreamReader sr = new StreamReader(Server.MapPath("~/App_Data/ReportSamples/通用表單_日.xml"));//檔案放在App_Data裡面
+
+            myDoc = sr.ReadToEnd();      //從頭讀到尾
+
+            sr.Close();
+
+            //下面開始抓資料
+
+            //if (asset != null)
+            //{
+            //    //將抓到的資料，替換掉我們剛剛設的文字
+            //    myDoc = myDoc.Replace("UserDpt", dptName);
+            //    myDoc = myDoc.Replace("AccDpt", asset.purchase.AccDptUid);
+            //    myDoc = myDoc.Replace("BmedNo", asset.bmedm.BmedNo);
+            //    myDoc = myDoc.Replace("EYAssetNo", asset.budget.EYAssetNo);
+            //    myDoc = myDoc.Replace("assetname", asset.bmedm.ACname);
+            //    myDoc = myDoc.Replace("brand", asset.purchase.Brand + "/" + asset.purchase.Type);
+            //    myDoc = myDoc.Replace("uprice", asset.purchase.UPrice.ToString());
+            //}
+            //寫出
+            Response.Write(myDoc);
+            Response.End();
+
+            return View();
+        }
+
+        public ActionResult GetMonthReportWord()
+        {
+            string fileName = "日報表";
+            Response.Clear();
+            Response.AddHeader("content-disposition", "attachment;filename=" + fileName + ".doc"); //fileName是word的檔名
+            Response.ContentEncoding = System.Text.Encoding.GetEncoding("utf-8");     //編碼utf-8
+            Response.ContentType = "application/vnd.ms-word";                         //讓瀏覽器知道是word檔
+            Response.Charset = "";
+
+            string myDoc = "";
+            StreamReader sr = new StreamReader(Server.MapPath("~/App_Data/ReportSamples/通用表單_日.xml"));//檔案放在App_Data裡面
+
+            myDoc = sr.ReadToEnd();      //從頭讀到尾
+
+            sr.Close();
+
+            //下面開始抓資料
+
+            //if (asset != null)
+            //{
+            //    //將抓到的資料，替換掉我們剛剛設的文字
+            //    myDoc = myDoc.Replace("UserDpt", dptName);
+            //    myDoc = myDoc.Replace("AccDpt", asset.purchase.AccDptUid);
+            //    myDoc = myDoc.Replace("BmedNo", asset.bmedm.BmedNo);
+            //    myDoc = myDoc.Replace("EYAssetNo", asset.budget.EYAssetNo);
+            //    myDoc = myDoc.Replace("assetname", asset.bmedm.ACname);
+            //    myDoc = myDoc.Replace("brand", asset.purchase.Brand + "/" + asset.purchase.Type);
+            //    myDoc = myDoc.Replace("uprice", asset.purchase.UPrice.ToString());
+            //}
+            //寫出
+            Response.Write(myDoc);
+            Response.End();
+
+            return View();
+        }
+
     }
 }
---
+++ InspectSystem/InspectSystem/Models/ReportQryVModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InspectSystem.Models
+{
+    public class ReportQryVModel
+    {
+        [Display(Name = "區域")]
+        public int AreaId { get; set; }
+        [Display(Name = "週期")]
+        public int CycleId { get; set; }
+        [Display(Name = "類別")]
+        public int ClassId { get; set; }
+        [Display(Name = "表單申請日(起)")]
+        public DateTime ApplyDateFrom { get; set; }
+        [Display(Name = "表單申請日(至)")]
+        public DateTime ApplyDateTo { get; set; }
+    }
+}
